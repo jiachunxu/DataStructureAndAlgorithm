42f8ea76654d24247e67a0d9fe8018a68ebb4046
jiacx <954951185@qq.com>
2023-02-15 17:43:05 +0800

    冒泡排序

---
+++ src/main/java/org/jsw/algorithm/冒泡排序.java
@@ -0,0 +1,30 @@
+package org.jsw.algorithm;
+
+import java.util.Arrays;
+
+public class 冒泡排序 {
+
+    public static void main(String[] args) {
+
+        int[] num = {4, 69, 3, 3, 1, 56};
+
+        bubbleSort(num);
+
+        System.out.println(Arrays.toString(num));
+
+
+    }
+
+    public static void bubbleSort(int[] num) {
+        int len = num.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (num[j] > num[j + 1]) {
+                    int temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
