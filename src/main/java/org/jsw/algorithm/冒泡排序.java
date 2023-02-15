198ab6b26c2eb4aa190f19465fcc84e1bf52ff3f
jiacx <954951185@qq.com>
2023-02-15 22:08:42 +0800

    update 冒泡

--- src/main/java/org/jsw/algorithm/冒泡排序.java
+++ src/main/java/org/jsw/algorithm/冒泡排序.java
@@ -16,6 +16,9 @@ public class 冒泡排序 {
     }
 
     public static void bubbleSort(int[] num) {
+        if (num == null) {
+            return;
+        }
         int len = num.length;
         for (int i = 0; i < len - 1; i++) {
             for (int j = 0; j < len - 1 - i; j++) {
