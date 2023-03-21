16d9e19a0daed886dab2838691808b1d2f6c5471
jiacx <954951185@qq.com>
2023-03-21 18:22:29 +0800

    update 冒泡

---
+++ src/main/java/org/jsw/algorithm/选择排序.java
@@ -0,0 +1,35 @@
+package org.jsw.algorithm;
+
+import java.util.Arrays;
+
+public class 选择排序 {
+
+    public static void main(String[] args) {
+
+        int[] num = {4, 69, 3, 3, 1, 56};
+
+        selectSort(num);
+
+        System.out.println(Arrays.toString(num));
+
+
+    }
+
+    public static void selectSort(int[] num) {
+        if (num == null) {
+            return;
+        }
+        int len = num.length;
+        int temp;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (num[i] > num[j]) {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+
+    }
+}
